c2bb120164c4fdded7cd027a1211e01bc72b4c4c
KimMiruJin <31152400+KimMiruJin@users.noreply.github.com>
2021-01-18 21:26:18 +0900

    TEST2

--- src/main/java/com/MDM/mdm/MdmApplication.java
+++ src/main/java/com/MDM/mdm/MdmApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MdmApplication {
 	//main commit test
+	// TEST2
 	public static void main(String[] args) {
 		SpringApplication.run(MdmApplication.class, args);
 	}
